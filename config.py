f9094e13aa0328c7fbb9da407c55a4dce63e9508
shadowmonarchop123-coder <shadowmonarchop123@gmail.com>
2026-05-15 20:13:13 +0530

    Fix API_KEY string formatting in config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ class Config:
         self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+NJ9Vftkc10g2NmI1")
 
         self.API_URL = getenv("API_URL", "https://api.fallenapi.fun")
-        self.API_KEY = getenv("API_KEY", fa1ae4_ImqKL2Kqfu9LLPzx-Ni0BfE9nXCSVxzG) # Get this value from @FallenApiBot by sending /apikey
+        self.API_KEY = getenv("API_KEY", "fa1ae4_ImqKL2Kqfu9LLPzx-Ni0BfE9nXCSVxzG") # Get this value from @FallenApiBot by sending /apikey
 
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", "False").lower() == "true"
         self.AUTO_END: bool = getenv("AUTO_END", "False").lower() == "true"
